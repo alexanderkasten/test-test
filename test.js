837a210781624d6979aff2732823a280100687b3
Alex <alexanderkasten@users.noreply.github.com>
2024-08-14 21:56:36 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,4 @@ console.log('this', this);
 console.log('globalThis', globalThis);
 console.log('currentScript', document.currentScript);
 console.log('import meta', import.meta);
+console.log(new URL(import.meta.url).searchParams.get("test"));
