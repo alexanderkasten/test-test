f33ef0861aaf02f5bb00a880e885d7d7141cd2c6
Alex <alexanderkasten@users.noreply.github.com>
2024-08-14 21:49:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ console.log('test');
 console.log('this', this);
 console.log('globalThis', globalThis);
 console.log('currentScript', document.currentScript);
+console.log('import meta', import.meta);
